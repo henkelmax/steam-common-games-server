27131c92b8aca724f7ca35a2d620d2ada7af3a5a
Max Henkel <max@maxhenkel.de>
2020-09-19 21:47:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const http = require('https')
+const querystring = require('querystring')
+
+const PORT = process.env.PORT || 5000
+const API_KEY = process.env.API_KEY || ''
+
+const app = express()
+app.use(cors())
+
+app.get('*', (req, res) => {
+    req.query.key = API_KEY
+    const options = {
+        host: 'api.steampowered.com',
+        protocol: 'https:',
+        port: 443,
+        path: `${req.path}?${querystring.stringify(req.query)}`,
+        method: 'GET'
+    }
+
+    const creq = http.request(options, pres => {
+        pres.setEncoding('utf8')
+        res.writeHead(pres.statusCode)
+        pres.on('data', chunk => {
+            res.write(chunk)
+        })
+        pres.on('close', () => {
+            res.end()
+        })
+        pres.on('end', () => {
+            res.end()
+        })
+    }).on('error', e => {
+        console.error(e.message)
+        try {
+            res.writeHead(500)
+            res.write(e.message)
+        } catch (e) { }
+        res.end()
+    })
+    creq.end()
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
